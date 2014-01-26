a962b796c6d7a8a0bd889e0f02e915c0abfca525
paninapress <NPacifico25@gmail.com>
2014-01-25 19:29:49 -0800

    laid out methods

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,21 @@
 class LinksController < ApplicationController
+
+	#Display history of links creates
+	def index
+		@links = Link.all
+	end
+
+	#New instance of a link
+	def new
+		@link = Link.new
+	end
+
+	#Add new link to database
+	def create
+		
+	end
+
+	def show
+	end
+
 end
