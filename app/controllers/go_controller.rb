2750753c4266270fcf13df23690bb9fbc19e1852
paninapress <NPacifico25@gmail.com>
2014-01-26 20:40:42 -0800

    added bootstrap via RailsApps Project

--- app/controllers/go_controller.rb
+++ app/controllers/go_controller.rb
@@ -4,9 +4,21 @@ def index
 end
 
 def redirect
+	link = Link.find_by(:random_string => params[:random_string])
+	
+	x = link.url
+
+	if x.match("http://") != nil
+	        redirect_to "#{x}"
+	elsif x.match("www.") != nil
+	        redirect_to "http://" + "#{x}"
+	else
+	        redirect_to "http://www." + "#{x}"
+	end
 end
 
 def preview
+	@total_count = Link.count
 end
 
 
